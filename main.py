5453ade7b471b2f8a6dc819a9d66e31182aa15dd
Dina Mazloum <dinamazloum6@gmail.com>
2025-04-07 08:53:56 +0000

    sea_level_predictor

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import sea_level_predictor
-from unittest import main
+from sea_level_predictor import draw_plot
 
-# Test your function by calling it here
-sea_level_predictor.draw_plot()
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
+# Generate the plot and save the image
+draw_plot()
--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -3,21 +3,34 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
 def draw_plot():
-    # Read data from file
+    # 1. Read the data
+    df = pd.read_csv("epa-sea-level.csv")
 
+    # 2. Create scatter plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], label='Original Data')
 
-    # Create scatter plot
+    # 3. Line of best fit (1880 to 2050)
+    full_reg = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    x_full = pd.Series(range(1880, 2051))
+    y_full = full_reg.slope * x_full + full_reg.intercept
+    plt.plot(x_full, y_full, color='blue', label='Best Fit: All Data')
 
+    # 4. Line of best fit (2000 to 2050)
+    df_recent = df[df['Year'] >= 2000]
+    recent_reg = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
+    x_recent = pd.Series(range(2000, 2051))
+    y_recent = recent_reg.slope * x_recent + recent_reg.intercept
+    plt.plot(x_recent, y_recent, color='red', label='Best Fit: 2000+ Data')
 
-    # Create first line of best fit
+    # 5. Customize plot
+    plt.xlabel("Year")
+    plt.ylabel("Sea Level (inches)")
+    plt.title("Rise in Sea Level")
+    plt.legend()
 
-
-    # Create second line of best fit
-
-
-    # Add labels and title
-
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
-    plt.savefig('sea_level_plot.png')
-    return plt.gca()
+    # 6. Save and return figure
+    plt.tight_layout()
+    fig = plt.gcf()
+    fig.savefig('sea_level_plot.png')
+    return fig
